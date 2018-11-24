09fe03b79bb4a02dcaaedf6a46a35b23df15adec
PishkaPashka <tankist3001-sergant@mail.ru>
2018-11-24 22:11:04 +0700

    TWO

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <windows.h>
+#include <locale>
 int plus(int a, int b);
+int check();
 
 int main(double a, double b)
 {
@@ -9,5 +11,21 @@ int main(double a, double b)
 
 int plus(int a, int b)
 {
+	a = check();
+	b = check();
 	c = a + b;
+}
+
+int check()
+{
+	while (1)
+	{
+		printf("Enter data:  ");
+		scanf_s("%c", &el);
+		if (isalpha(el) != 0 and isalnum(el))
+		{
+			return el;
+			break;
+		}
+	}
 }
